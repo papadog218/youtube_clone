c80332af81b4c24761264f8b8ca15ebe2066fa90
papadog218 <32761630+papadog218@users.noreply.github.com>
2020-05-27 14:06:14 +0900

    #2-19 완료

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -4,9 +4,16 @@ import { users, userDetail, editProfile, changePassword } from "../controllers/u
 
 const userRouter = express.Router();
 
-userRouter.get(routes.home, users);
-userRouter.get(routes.userDetail, userDetail);
+// userRouter.get(routes.home, users);
+
+/*
+    라우터 순서가 중요함
+    --> 프로필수정이 유저 디테일 보다 아래에 있었더니 프로필수정으로 들어갔는데
+        디테일 화면이 나오는 버그가 있었음
+*/
+
 userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.userDetail, userDetail);
 userRouter.get(routes.changePassword, changePassword);
 
 export default userRouter;
