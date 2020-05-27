142b2d4b575e6616b7519544b2242da0921837e1
papadog218 <32761630+papadog218@users.noreply.github.com>
2020-05-27 11:34:20 +0900

    #2-19 까지 완성

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,5 +1,10 @@
 export const home = (req, res) => res.render("home", { pageTitle:"Home" });
-export const search = (req, res) => res.render("search", { pageTitle:"Search" });
+export const search = (req, res) => {
+    // console.log(req.query.term);
+    // const keyword = req.query.term; // ES6 이전 방식
+    const {query: { term: keyword} } = req; // ES6 방식
+    res.render("search", { pageTitle:"Search", keyword });
+};
 export const videos = (req, res) => res.render("videos", { pageTitle:"Videos" });
 export const upload = (req, res) => res.render("upload", { pageTitle:"Upload" });
 export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle:"Video Detail" });
