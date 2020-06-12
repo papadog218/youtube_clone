34ce3a71650dd438b5f1a529eb683bbd78de01e4
papadog218 <32761630+papadog218@users.noreply.github.com>
2020-06-13 01:53:54 +0900

    실제 몽고디비 연결코드

--- db.js
+++ db.js
@@ -1,38 +1,6 @@
-export const videos = [
-    {
-        id: 324393,
-        title: "title1",
-        description: "This is Blah blah1",
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 940830,
-            name: "yhKim1",
-            email: "mki218@naver.com"
-        }
-    },
-    {
-        id: 324394,
-        title: "title2",
-        description: "This is Blah blah2",
-        views: 25,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 940830,
-            name: "yhKim2",
-            email: "mki218@naver.com"
-        }
-    },
-    {
-        id: 324395,
-        title: "title3",
-        description: "This is Blah blah3",
-        views: 26,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 940830,
-            name: "yhKim3",
-            email: "mki218@naver.com"
-        }
-    }
-]
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://locahost:27017/wetube", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
