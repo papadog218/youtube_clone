121714d1d649fd4ec1ae183e328d3075cc2dfcd8
papadog218 <32761630+papadog218@users.noreply.github.com>
2020-08-07 10:00:02 +0900

    #3.7 중간저장

--- middlewares.js
+++ middlewares.js
@@ -2,20 +2,20 @@ import multer from "multer";
 import routes from "./routes";
 
 const myStorage = multer.diskStorage({
-    // 업로드할 파일 저장 위치
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/videos/')
-    },
-    // 업로드될 파일이름 설정
-    filename: function(req, file, cb) {
-        let extension = file.originalname;
-        cb(null, extension)
-    }
+  // 업로드할 파일 저장 위치
+  destination: function (req, file, cb) {
+    cb(null, "uploads/videos/");
+  },
+  // 업로드될 파일이름 설정
+  filename: function (req, file, cb) {
+    let extension = file.originalname;
+    cb(null, extension);
+  },
 });
 
-const multerVideo = multer({storage: myStorage});
+const multerVideo = multer({ storage: myStorage });
 // const multerVideo = multer({dest: "videos/"});
- 
+
 // const multerVideo = multer({
 //     storage: multerS3({
 //         s3,
@@ -36,14 +36,14 @@ const multerVideo = multer({storage: myStorage});
 // });
 
 export const localMiddleware = (req, res, next) => {
-    res.locals.siteName = "WeTube";
-    res.locals.routes = routes;
-    res.locals.user = {
-        isAuthenticated: true,
-        id: 1
-    };
+  res.locals.siteName = "WeTube";
+  res.locals.routes = routes;
+  res.locals.user = {
+    isAuthenticated: true,
+    id: 1,
+  };
 
-    next();
+  next();
 };
 
 export const uploadVideo = multerVideo.single("videoFile");
